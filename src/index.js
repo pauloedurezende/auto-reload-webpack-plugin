e3e9554ecf83f12acc2f0949e7789936186b4830
Paulo Eduardo Rezende <rezendepauloeduardo@outlook.com>
2018-11-23 18:46:37 -0200

    feat: add auto reload plugin

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { resolve } = require('path');
+const cluster = require('cluster');
+const process = require('process');
+
+class AutoReloadWebpackPlugin {
+  constructor({ filePath }) {
+    this.filePath = filePath;
+    this.currentWorker = null;
+  }
+
+  apply(compiler) {
+    cluster.setupMaster({
+      exec: resolve(process.cwd(), this.filePath)
+    });
+
+    cluster.on('online', (worker) => {
+      this.currentWorker = worker;
+    });
+
+    cluster.on('exit', () => {
+      cluster.fork();
+    });
+
+    compiler.hooks.afterEmit.tap('AutoReloadWebpackPlugin', () => {
+      if (this.currentWorker) {
+        try {
+          process.kill(this.currentWorker.process.pid, 'SIGTERM');
+        } catch (e) {
+          console.warn('Could not terminate this process');
+        }
+      }
+
+      if (!this.currentWorker) {
+        cluster.fork();
+      }
+    });
+  }
+}
+
+module.exports = AutoReloadWebpackPlugin;
